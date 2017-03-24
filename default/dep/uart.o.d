cc67042bc79fdd5b64ca06652f7cf0ba375d6899
martin075 <vaskoma@gmail.com>
2017-03-24 09:03:21 +0100

    b

---
+++ default/dep/uart.o.d
@@ -0,0 +1,50 @@
+uart.o: ../uart.c c:\winavr-20100110\avr\include\avr\io.h \
+ c:\winavr-20100110\avr\include\avr\sfr_defs.h \
+ c:\winavr-20100110\avr\include\inttypes.h \
+ c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stdint.h \
+ c:\winavr-20100110\avr\include\stdint.h \
+ c:\winavr-20100110\avr\include\avr\iom168.h \
+ c:\winavr-20100110\avr\include\avr\iomx8.h \
+ c:\winavr-20100110\avr\include\avr\portpins.h \
+ c:\winavr-20100110\avr\include\avr\common.h \
+ c:\winavr-20100110\avr\include\avr\version.h \
+ c:\winavr-20100110\avr\include\avr\fuse.h \
+ c:\winavr-20100110\avr\include\avr\lock.h \
+ c:\winavr-20100110\avr\include\string.h \
+ c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stddef.h \
+ c:\winavr-20100110\avr\include\stdio.h \
+ c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stdarg.h ../uart.h
+
+c:\winavr-20100110\avr\include\avr\io.h:
+
+c:\winavr-20100110\avr\include\avr\sfr_defs.h:
+
+c:\winavr-20100110\avr\include\inttypes.h:
+
+c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stdint.h:
+
+c:\winavr-20100110\avr\include\stdint.h:
+
+c:\winavr-20100110\avr\include\avr\iom168.h:
+
+c:\winavr-20100110\avr\include\avr\iomx8.h:
+
+c:\winavr-20100110\avr\include\avr\portpins.h:
+
+c:\winavr-20100110\avr\include\avr\common.h:
+
+c:\winavr-20100110\avr\include\avr\version.h:
+
+c:\winavr-20100110\avr\include\avr\fuse.h:
+
+c:\winavr-20100110\avr\include\avr\lock.h:
+
+c:\winavr-20100110\avr\include\string.h:
+
+c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stddef.h:
+
+c:\winavr-20100110\avr\include\stdio.h:
+
+c:\winavr-20100110\lib\gcc\avr\4.8.1\include\stdarg.h:
+
+../uart.h:
